531e5dddbfbe4b6f0f50f9f0c6028a4ee7a5bcec
Lyna Asma ADJAL <lynaasma.adjal@etu.usthb.dz>
2026-06-30 19:01:53 +0100

    completed phonebook step 6

--- practice/practice/src/App.jsx
+++ practice/practice/src/App.jsx
@@ -1,41 +1,64 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
+  // states initialisation
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+  // effect call
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }, [])
+
+
+
   console.log('render', notes.length, 'notes')
 
+
+  //notes to show check before rendering App component
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
 
+
+  // event handlers 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+  }
+
+  // form submission event handler 
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
+      important: Math.random() < 0.5
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    // server communication
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        console.log(response)
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+
+
   }
 
   return (
@@ -48,7 +71,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
       <form onSubmit={addNote}>
--- practice/practice/src/components/Note.jsx
+++ practice/practice/src/components/Note.jsx
@@ -1,7 +1,13 @@
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? 'make not important' : 'make important'
 
-const Note = ({ note }) => {
-    console.log('Note prop received from the array', note)
-    return < li > {note.content} </li >
+  return (
+    <li>
+      {note.content} 
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
 }
 
 export default Note
