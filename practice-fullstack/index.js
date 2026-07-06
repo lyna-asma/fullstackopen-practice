313a7b036eaf5af3099189aee52fd736fb72b078
Lyna Asma ADJAL <lynaasma.adjal@etu.usthb.dz>
2026-07-06 03:12:46 +0100

    fixed the index js file

--- practice-fullstack/index.js
+++ practice-fullstack/index.js
@@ -1,69 +1,15 @@
-
 require('dns').setServers(['8.8.8.8', '1.1.1.1'])
 require('dotenv').config()
 const express = require('express')
-const Note = require('./models/note')
 const morgan = require('morgan')
-require('dotenv').config()
-
-const mongoose = require('mongoose')
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const password = process.argv[2]
-const url = `mongodb+srv://fullstackopen:${password}@cluster0.fczz5gk.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url, { family: 4 })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-
-
+const Note = require('./models/note')
 
 const app = express()
-
-
-
 app.use(express.json())
-
 app.use(express.static('dist'))
-// custom us eof morgan instead of using the 'tiny ' style 
 
-morgan.token('body', (req) => {
-  return JSON.stringify(req.body)
-})
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-/*
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-*/
-
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
@@ -71,45 +17,31 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-
 app.get('/api/notes/:id', (request, response) => {
   Note.findById(request.params.id).then(note => {
     response.json(note)
   })
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
-
   if (!body.content) {
     return response.status(400).json({ error: 'content missing' })
   }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
-
   note.save().then(savedNote => {
     response.json(savedNote)
   })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id).then(() => {
+    response.status(204).end()
+  })
 })
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
