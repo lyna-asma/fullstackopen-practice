369c7babb99ce53c3e53c00f4796bf9072891303
Lyna Asma ADJAL <lynaasma.adjal@etu.usthb.dz>
2026-07-06 02:30:38 +0100

    part3 c functions with db

--- practice-fullstack/index.js
+++ practice-fullstack/index.js
@@ -1,9 +1,39 @@
-const express = require('express')
 
+require('dns').setServers(['8.8.8.8', '1.1.1.1'])
+require('dotenv').config()
+const express = require('express')
+const Note = require('./models/note')
 const morgan = require('morgan')
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const password = process.argv[2]
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.fczz5gk.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
 
 
 const app = express()
+
+
+
 app.use(express.json())
 
 app.use(express.static('dist'))
@@ -14,6 +44,7 @@ morgan.token('body', (req) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+/*
 let notes = [
   {
     id: "1",
@@ -31,25 +62,20 @@ let notes = [
     important: true
   }
 ]
-
-
+*/
 
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
+
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-
-
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 const generateId = () => {
@@ -59,25 +85,24 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
+
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
   notes = notes.filter(note => note.id !== id)
