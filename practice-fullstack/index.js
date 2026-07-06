70406e7747f778e0d7dbecb88134dfa006b08c56
Lyna Asma ADJAL <lynaasma.adjal@etu.usthb.dz>
2026-07-06 18:45:41 +0100

    mongo db set up for my post and gte all req

--- practice-fullstack/index.js
+++ practice-fullstack/index.js
@@ -1,24 +1,39 @@
+// this is to fix DNS issue 
 require('dns').setServers(['8.8.8.8', '1.1.1.1'])
+// to use env
 require('dotenv').config()
+// some important imports s
 const express = require('express')
 const morgan = require('morgan')
 const Note = require('./models/note')
 
+// app instance creation
 const app = express()
+
+// json parser middleware for the req.body to be used
 app.use(express.json())
+// static middleware to display the frontend files from /dist directory
 app.use(express.static('dist'))
 
+// morgan for logging some details to console useful fro debugging
+// , notice we first add body token to the rest bcz it doesn t come by default 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+
+// route handlers 
+
+// get all route handler using find methos from mongo model Note , notice the notes we get are note+s , this matches the doc  collection name mongo creates for convenience 
+app.get('/api/notes', (request, response, next) => {
+  Note.find({})
+    .then(notes => {
+      response.json(notes)
+    })
+    .catch(error => next(error))
 })
 
 // with not found error in case we  look for id non existant
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
 
@@ -29,14 +44,11 @@ app.get('/api/notes/:id', (request, response) => {
       }
     })
 
-    .catch( error => next(error)
-
-      // The 400 (Bad Request) status code indicates that the server cannot or will not process the request due to something that
-      //  is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).
-     
-    )
+    .catch( error => next(error))
 })
-app.post('/api/notes', (request, response) => {
+
+// create new note hadler notice the use of  Note() constructor + save()
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
   if (!body.content) {
     return response.status(400).json({ error: 'content missing' })
@@ -45,11 +57,14 @@ app.post('/api/notes', (request, response) => {
     content: body.content,
     important: body.important || false,
   })
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
+// delete handler with mongo model method ( 2 successfull cases either delete when non existant note id , or delete an existant one)
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then(result => {
@@ -58,6 +73,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+
+// update handler with mongo model methods (notice the use of findById() + save() instead of findByIdAndUpdate())
 app.put('/api/notes/:id', (request, response, next) => {
   const { content, important } = request.body
 
@@ -77,6 +94,30 @@ app.put('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+
+
+// before the last middleware => unknowreqhandler
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+// its use
+app.use(unknownEndpoint)
+
+
+/// last middleware => errorhandler 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+//its use
+app.use(errorHandler)
+
+// laaast one of all => the built i error handler from express ....(no code fro it)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- practice-fullstack/models/note.js
+++ practice-fullstack/models/note.js
@@ -1,13 +1,17 @@
+// importing the library necessary for the comm with the cluster 
+// we use mongoose instead of mongodb for some prefrences
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-
+// we get the url value from the env variable
 const url = process.env.MONGODB_URI
 
+// logging to console
 console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
 
+// connecting wih the url we specify i env + consition to only accept IP4
+mongoose.connect(url, { family: 4 })
   .then(result => {
     console.log('connected to MongoDB')
   })
@@ -15,11 +19,14 @@ mongoose.connect(url, { family: 4 })
     console.log('error connecting to MongoDB:', error.message)
   })
 
+  // creation of the schema for our model 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
 
+// toJSON transform to format the objects returned by Mongoose
+// replaces _id with id (as a string) and removes __v (versioning field)
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -28,5 +35,6 @@ noteSchema.set('toJSON', {
   }
 })
 
-
+// assigning the Note model the noteSchema schema
+// mongoose will automatically create a collection named 'notes' (plural of 'Note')
 module.exports = mongoose.model('Note', noteSchema)
